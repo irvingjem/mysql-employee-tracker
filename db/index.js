911829f6f0357d32a51c134aac031685f7838a86
Your Name <irving.jem@gmail.com>
2022-04-26 10:43:23 -0700

    added comments for functions to add to database to create SQL functions

--- db/index.js
+++ db/index.js
@@ -2,7 +2,27 @@
 const connection = require('./connection');
 
 // Create class to store all data
-class Database {}
+class Database {
+    // add connection
+    constructor(connection) {
+        this.connection = connection;
+    };
+
+    // add all functions from inquirer 
+    // All departments
+    // All roles
+    // All employees
+    // All managers
+    // All departments
+    // Add departments
+    // Delete Departments
+    // Add role
+    // Delete role
+    // Add employee
+    // Delete employee
+    // Update employee role
+    // Update manager role
+}
 
 
 // exporter for other files
