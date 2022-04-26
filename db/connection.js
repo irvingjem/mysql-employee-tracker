cb42ad243bb61879c495bb6e061c3f7b9b692464
Your Name <irving.jem@gmail.com>
2022-04-26 10:36:54 -0700

    moved db connection to its own file for clarification

---
+++ db/connection.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+// Connect to database
+const db = mysql.createConnection({
+        host: 'localhost',
+        // Your MySQL username,
+        user: process.env.DB_USER,
+        // Your MySQL password
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
+    },
+    console.log('Connected to the employee tracker database.')
+
+);
+
+connection.connect(function(err) {
+    if (err) throw err;
+});
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -2,22 +2,6 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 
-require('dotenv').config();
-
-// Connect to database
-const db = mysql.createConnection({
-        host: 'localhost',
-        // Your MySQL username,
-        user: process.env.DB_USER,
-        // Your MySQL password
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME
-    },
-    console.log('Connected to the employee tracker database.')
-
-);
-
-
 function userPrompts() {
     inquirer.prompt(
             [{
